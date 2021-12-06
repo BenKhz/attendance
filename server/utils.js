be3fa7313d1681bed66f3cbb6011f7c63c16e54e
Ben Bernardy <benbernardy@gmail.com>
2021-12-06 11:35:09 -0800

    CHANGED: Logging middleware to include easy visibility into req objects.

--- server/utils.js
+++ server/utils.js
@@ -2,8 +2,9 @@
 
 module.exports = {
   log: (req, res, next) => {
-    console.log(`${req.method} recieved at ${req.originalUrl}
-                Params: ${req.params}`)
+    console.log(`${req.method} recieved at ${req.originalUrl}\n Query string: ${Object.entries(req.query)}\n Params : ${Object.entries(req.params)}`)
+    req.body ? console.log(` Body: ${Object.entries(req.body)}`) : console.log(" Body : ");
+    req.data ? console.log(` Data: ${Object.entries(req.data)}`) : console.log(" Data : ");
     next();
   }
 }
