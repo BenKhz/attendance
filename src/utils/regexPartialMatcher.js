4d0e398974d68e9f7b53d2bc22a4db835f9b1e71
Ben Bernardy <benbernardy@gmail.com>
2022-02-02 10:37:27 -0800

    Update Regex for broader matching

--- src/utils/regexPartialMatcher.js
+++ src/utils/regexPartialMatcher.js
@@ -1,19 +1,22 @@
-export default function partialRegexMatcher (recordName, displayName) {
+export default function partialRegexMatcher(recordName, displayName) {
   // prune contents from square brackets and all bracket characters from display name
-  var displayName = displayName.replace(/(\[.*?\] *)|[\(]|[\)]/ig, '')
+  var displayName = displayName.replace(/(\[.*?\] *)|[\(]|[\)]/ig, '').replace(/(hr.?lax?.?\d*)/ig, '')
   var recordName = recordName.replace(/(\[.*?\] *)|[\(]|[\)]/ig, '')
   displayName = displayName.toLowerCase().split(' ')
   recordName = recordName.toLowerCase().split(' ')
-  for(var word of displayName) {
-    if(!recordName.includes(word))
-    return false
+  console.log("Display Name :  ", displayName)
+  console.log("Recorded Name :  ", recordName)
+  for (var word of displayName) {
+    if (word.length && !recordName.includes(word)) {
+      return false
+    }
   }
-  return true
+  return true;
 }
 
 // -------- Test Suite ------------
 
-// var successCases =[
+// var successCases = [
 //   ['[HRLAX49] Ben Bernardy', 'Ben Bernardy', 'full match with HRLAX prepend'],
 //   ['[HRLAX 49] Ben Bernardy', 'Ben Bernardy', 'HR prepended badge with whitespace'],
 //   ['[HRLAX49] Benji Bernardy', 'Ben (Benji) Bernardy', 'full match with preferred nickname in parens'],
@@ -22,15 +25,15 @@ export default function partialRegexMatcher (recordName, displayName) {
 //   ['HR-LA-49 Ben Bernardy', "Ben Bernardy", "HRLAX prepended outside of square brackets with multiple hyphens and no X"],
 //   ['[HR-LAX] Ben (Benji) Bernardy', 'ben (Benji) BeRnardy', 'preferred name included in display parens'],
 // ]
-// var failCases =[
-//   ['[HRLAX49] Benny Bernardy', 'Ben (Benji)Bernardy', 'Display name does not match recorded preferred name'],
+// var failCases = [
+//   ['[HRLAX49] Benny Bernardy', 'Ben (Benji) Bernardy', 'Display name does not match recorded preferred name'],
 // ]
 
 // successCases.forEach(test => {
-//   console.log(`Condition: ${test[2]} : display name ${test[0]}, and recorded name ${test[1]}`)
-//   console.log( '-------->', partialRegexMatcher(test[0], test[1]))
+//   console.log(`SHOULD PASS: ${test[2]} : display name ${test[1]}, and recorded name ${test[0]}`)
+//   console.log('-------->', partialRegexMatcher(test[1], test[0]))
 // })
 // failCases.forEach(test => {
-//   console.log(`Condition: ${test[2]} : display name ${test[0]}, and recorded name ${test[1]}`)
-//   console.log( '-------->', !partialRegexMatcher(test[0], test[1]))
+//   console.log(`Should FAIL: ${test[2]} : display name ${test[1]}, and recorded name ${test[0]}`)
+//   console.log('-------->', partialRegexMatcher(test[1], test[0]))
 // })
